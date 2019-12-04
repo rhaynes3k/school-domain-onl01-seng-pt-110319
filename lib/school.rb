152fa79bc35380545735b499234a9789b7e7a157
Rommel Haynes <rhaynes3k@gmail.com>
2019-12-04 03:41:51 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,46 @@
+class School
+  def initialize(sch)
+    roster = {}
+    @school = sch
+    @roster = roster
+#binding.pry
+  end
+  
+  def roster
+    @roster
+  end
+  
+  def add_student(name, grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << name
+    else
+    @roster[grade] = []
+    @roster[grade] << name
+    end
+  end  
+
+  def grade(grade)
+    n_arr = []
+    @roster[grade].each do |n|
+       n_arr.push(n)     
+    end
+    n_arr
+  end
+  
+  def sort
+   @roster.each do |g, n|
+     n.sort
+   end
+   
+ end 
+end
+
+
+
+
+
+
+
+
+
 # code here!
